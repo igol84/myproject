86ca5ddd8cd4581e34496877c700f62b253023ce
igolu <igolub84@gmail.com>
2020-08-04 15:35:41 +0300

    produc_desc, money, covert price

--- prjstore/domain/product_desc.py
+++ prjstore/domain/product_desc.py
@@ -67,7 +67,7 @@ class ProductDesc(metaclass=ABCMeta):
     price = property(get_price, set_price)
 
     def convert_price(self):
-        self._price = MoneyMy.get_converted_Money(self._price)
+        self._price = MoneyMy.get_converted_money(self._price)
 
     def get_desc(self):
         return self._desc
--- util/money_my.py
+++ util/money_my.py
@@ -9,12 +9,12 @@
     >>> print(money.format('uk_UA'))                   # Get format price
     750,50 ₴
 
-    >>> convert_money = MoneyMy.get_converted_Money(money)     # New converted price+
+    >>> convert_money = MoneyMy.get_converted_money(money)     # New converted price+
     >>> print(convert_money)
     USD 27.29
     >>> print(convert_money.format('en_US'))           # Get format price
     $27.29
-    >>> money = MoneyMy.get_converted_Money(convert_money)     # Reverse
+    >>> money = MoneyMy.get_converted_money(convert_money)     # Reverse
     >>> print(money)
     UAH 750.50
 """
@@ -27,9 +27,9 @@ class MoneyMy(Money):
     default_currency = 'UAH'
     rate_USD_to_UAH = 27.5
 
-
-    def get_converted_Money(money):
-        if (money.currency == 'UAH'):
+    @staticmethod
+    def get_converted_money(money):
+        if money.currency == 'UAH':
             currency = 'USD'
             amount = money.amount / Decimal(money.rate_USD_to_UAH)
         else:
