85007dcff5aa7217b7f8067dbac1e90fb0165b3c
igolu <igolub84@gmail.com>
2020-08-12 11:47:41 +0300

    produc_desc, money, covert price, product_catalog, item

--- util/money_my.py
+++ util/money_my.py
@@ -7,7 +7,7 @@ class MoneyMy(Money):
 >>> money = MoneyMy(amount=750.5, currency='UAH')  # Create price
 >>> print(money)                                   # Get price
 UAH 750.50
->>> print(money.USD)                                   # Get price
+>>> print(money.USD)                               # Get price in USD
 USD 27.29
 >>> print(money.amount)                            # Get price amount
 750.5
@@ -19,7 +19,7 @@ UAH
 >>> convert_money = MoneyMy.get_converted_money(money)     # New converted price+
 >>> print(convert_money)
 USD 27.29
->>> print(convert_money.UAH)
+>>> print(convert_money.UAH)                       # Get price in UAH
 UAH 750.48
 >>> print(convert_money.format('en_US'))           # Get format price
 $27.29
@@ -27,31 +27,33 @@ $27.29
 >>> print(money)
 UAH 750.48
     """
-    default_currency = 'UAH'
+    currencies = ['UAH', 'USD']
+    default_currency = currencies[0]
     rate_USD_to_UAH = 27.5
 
-    def USD(self):
-        if self.currency == 'USD':
-            return self
-        elif self.currency == 'UAH':
-            return MoneyMy.get_converted_money(self)
-
-    USD = property(USD)
-
-    def UAH(self):
-        if self.currency == 'UAH':
-            return self
-        elif self.currency == 'USD':
-            return MoneyMy.get_converted_money(self)
-
-    UAH = property(UAH)
-
-    @staticmethod
-    def get_converted_money(money) -> 'MoneyMy':
-        if money.currency == 'UAH':
-            currency = 'USD'
-            amount = round(money.amount / Decimal(money.rate_USD_to_UAH), 2)
+    def __getattr__(self, item):
+        """For get money in .USD or .UAH"""
+        if item in MoneyMy.currencies:
+            get_currency = item
+            if get_currency == self.currency:
+                return self
+            else:
+                return MoneyMy.get_converted_money(self, get_currency)
         else:
-            currency = 'UAH'
-            amount = round(money.amount * Decimal(money.rate_USD_to_UAH), 2)
-        return MoneyMy(amount=amount, currency=currency)
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{item}'")
+
+    @classmethod
+    def get_converted_money(cls, money_my, to_currency=None) -> 'MoneyMy':
+        if money_my.currency == 'UAH':
+            to_currency = 'USD'
+            amount = round(money_my.amount / Decimal(money_my.rate_USD_to_UAH), 2)
+        else:
+            to_currency = 'UAH'
+            amount = round(money_my.amount * Decimal(money_my.rate_USD_to_UAH), 2)
+        return MoneyMy(amount=amount, currency=to_currency)
+
+
+if __name__ == '__main__':
+    money = MoneyMy(amount=750.5, currency='UAH')  # Create price
+    print(money)  # Get price
+    print(money.USD)  # Get price in USD
