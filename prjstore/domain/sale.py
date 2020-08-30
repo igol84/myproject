1bc195bbd36a1ce22732ee295a4db600b90be077
igolu <igolub84@gmail.com>
2020-08-30 16:11:59 +0300

    sale

--- prjstore/domain/sale.py
+++ prjstore/domain/sale.py
@@ -32,15 +32,18 @@ class Sale:
 <Sale: time: 06/06/2020, 12:19:55, not completed, line items:
 <SaleLineItem: item=<Item: product=<Product: id=4, desc=item5, price=UAH 300.00>, qty=1>, qty=1>>
 >>> sale.set_line_item(items[0])                                          # Add product to sale
+>>> sale.set_line_item(items[2])
 >>> sale
 <Sale: time: 06/06/2020, 12:19:55, not completed, line items:
 <SaleLineItem: item=<Item: product=<Product: id=4, desc=item5, price=UAH 300.00>, qty=1>, qty=1>
-<SaleLineItem: item=<Item: product=<Product: id=2, desc=item23, price=UAH 600.00>, qty=3>, qty=1>>
+<SaleLineItem: item=<Item: product=<Product: id=2, desc=item23, price=UAH 600.00>, qty=3>, qty=1>
+<SaleLineItem: item=<Item: product=<Product: id=6, desc=item2, price=UAH 500.00>, qty=2>, qty=1>>
 >>> sale.set_line_item(items[0], 2)                                          # Add same product to sale
 >>> sale
 <Sale: time: 06/06/2020, 12:19:55, not completed, line items:
 <SaleLineItem: item=<Item: product=<Product: id=4, desc=item5, price=UAH 300.00>, qty=1>, qty=1>
-<SaleLineItem: item=<Item: product=<Product: id=2, desc=item23, price=UAH 600.00>, qty=3>, qty=3>>
+<SaleLineItem: item=<Item: product=<Product: id=2, desc=item23, price=UAH 600.00>, qty=3>, qty=3>
+<SaleLineItem: item=<Item: product=<Product: id=6, desc=item2, price=UAH 500.00>, qty=2>, qty=1>>
 >>> sale['4']                                                    # get line item by product id "4"
 <SaleLineItem: item=<Item: product=<Product: id=4, desc=item5, price=UAH 300.00>, qty=1>, qty=1>
 >>> sale.completed()                                                                  # completed
@@ -49,20 +52,32 @@ True
 >>> sale.not_completed()                                                              # not completed
 >>> sale.is_complete()
 False
+>>> sale.unset_line_item_by_pr_id('2', 2)                                 # unset sale line pr id ='2'# items 3-2=1
 >>> sale.line_items                                                                   # get sale line items
 [<SaleLineItem: item=<Item: product=<Product: id=4, desc=item5, price=UAH 300.00>, qty=1>, qty=1>,\
- <SaleLineItem: item=<Item: product=<Product: id=2, desc=item23, price=UAH 600.00>, qty=3>, qty=3>]
+ <SaleLineItem: item=<Item: product=<Product: id=2, desc=item23, price=UAH 600.00>, qty=3>, qty=1>,\
+ <SaleLineItem: item=<Item: product=<Product: id=6, desc=item2, price=UAH 500.00>, qty=2>, qty=1>]
+
+>>> del sale['4']                                                             # del sale line item by product id='4'
+>>> sale.line_items
+[<SaleLineItem: item=<Item: product=<Product: id=2, desc=item23, price=UAH 600.00>, qty=3>, qty=1>,\
+ <SaleLineItem: item=<Item: product=<Product: id=6, desc=item2, price=UAH 500.00>, qty=2>, qty=1>]
+>>> sale.unset_line_item_by_pr_id('2')                           # unset sale line pr id ='2' items 1-1=0 -> del
+>>> sale
+<Sale: time: 06/06/2020, 12:19:55, not completed, line items:
+<SaleLineItem: item=<Item: product=<Product: id=6, desc=item2, price=UAH 500.00>, qty=2>, qty=1>>
+
 
 
 
     """
 
-    @contract
-    def __init__(self, pr: "isinstance(Item)", gty: "int, >0" = 1) -> None:
+    def __init__(self, pr=None, gty=1) -> None:
         self._list_sli = []
         self._is_complete = False
         self._time = datetime.datetime.now()
-        self.set_line_item(pr, gty)
+        if pr:
+            self.set_line_item(pr, gty)
 
     def get_list_sli(self):
         return self._list_sli
@@ -71,13 +86,31 @@ False
 
     @contract
     def set_line_item(self, item: "isinstance(Item)", qty: "int, >0" = 1) -> None:  # add new line items
-        if not self.is_already_set(item):  # is a new product in line items
+        if not self.is_item_already_set(item):  # is a new product in line items
             self._list_sli.append(SaleLineItem(item, qty))
         else:  # a same product in line items
             self[item.product.id].qty += qty
 
-    def is_already_set(self, item) -> bool:
+    @contract
+    def unset_line_item_by_pr_id(self, pr_id: "str", qty: "int, >0" = 1) -> None:  # unset line items
+        sli = self.get_line_item_by_pro_id(pr_id)
+        if sli and qty <= sli.qty:  # a same product in line items
+            if qty < sli.qty:
+                self[pr_id].qty -= qty
+            elif qty == sli.qty:
+                del self[pr_id]
+        else:
+            raise ValueError(f'quantity({qty}) should not be more than item.qty({self[pr_id].qty})!')
+
+    def is_item_already_set(self, item) -> bool:
         return item.product in self
+
+    @contract
+    def get_line_item_by_pro_id(self, pr_id: "str") -> SaleLineItem:
+        for sli in self._list_sli:
+            if sli.item.product.id == pr_id:
+                return sli
+        raise IndexError(f"Invalid product id: {pr_id}")
 
     def is_complete(self) -> bool:
         return self._is_complete
@@ -104,10 +137,12 @@ False
         return f"<{self.__class__.__name__}: time: {time}, {completed}, line items:{sale_line_items}>"
 
     def __getitem__(self, key):  # get line item by product id
-        for sli in self._list_sli:
+        return self.get_line_item_by_pro_id(key)
+
+    def __delitem__(self, key):  # del sale line item by product id
+        for i, sli in enumerate(self._list_sli):
             if sli.item.product.id == key:
-                return sli
-        raise IndexError(f"Invalid product id: {key}")
+                del self._list_sli[i]
 
     def __contains__(self, product):  # Does Sale contain this product in line items?
         for sli in self._list_sli:
