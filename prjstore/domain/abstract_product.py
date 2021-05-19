e423d33c6cf38ab6f005a411df2b5c3f25e56f65
igol84 <igolub84@gmail.com>
2021-05-19 10:54:48 +0300

    typing: abstract_product

--- prjstore/domain/abstract_product.py
+++ prjstore/domain/abstract_product.py
@@ -1,3 +1,5 @@
+from typing import Union, Optional
+
 from contracts import contract
 from abc import ABC, abstractmethod
 from util.money_my import MoneyMy, Decimal
@@ -9,21 +11,36 @@ class AbstractProduct(ABC):
     _default_curr = MoneyMy.default_currency
 
     @contract(id='str | int')
-    def __init__(self, id, name='item', price=0, currency=_default_curr):
+    def __init__(self, id: Union[str, int], name: str = 'item', price=0, currency=_default_curr):
         self._id = str(id)
         self.name = name
         self.set_price(price, currency)
 
+    ################################################################################################ id
     def get_id(self) -> str:
         return self._id
 
     id = property(get_id)
 
+    def get_name(self) -> str:
+        return self._name
+
+    ################################################################################################ name
+    @contract(name='str')
+    def set_name(self, name: str) -> None:
+        self._name = name
+
+    name = property(get_name, set_name)
+
+    ################################################################################################ price
     def get_price(self) -> MoneyMy:
         return self._price
 
     @contract(price='$MoneyMy | int | float | $Decimal', currency='None | str')
-    def set_price(self, price, currency=None) -> None:
+    def set_price(self,
+                  price: Union[MoneyMy | int | float | Decimal],
+                  currency: Optional[str] = None
+                  ) -> None:
         if isinstance(price, MoneyMy):
             self._price = price
         else:
@@ -32,17 +49,9 @@ class AbstractProduct(ABC):
 
     price = property(get_price, set_price)
 
+    ################################################################################################
     def convert_price(self, to_currency) -> None:
         self._price = MoneyMy.get_converted_money(self._price, to_currency)
-
-    def get_name(self) -> str:
-        return self._name
-
-    @contract(name='str')
-    def set_name(self, name) -> None:
-        self._name = name
-
-    name = property(get_name, set_name)
 
     def edit(self, name=None, price=None, currency=None) -> None:
         if name:
