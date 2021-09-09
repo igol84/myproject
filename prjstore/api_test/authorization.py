16d390a528bb4be741ffbabd59eaa9ffa6e01a16
igol84 <igolub84@gmail.com>
2021-09-09 16:26:54 +0300

    db

--- prjstore/api_test/authorization.py
+++ prjstore/api_test/authorization.py
@@ -4,6 +4,6 @@ host = 'http://31.148.245.50'
 username = 'qwe'
 password = 'qwe'
 
-r = requests.post(f'{host}/login', data={'username':username, 'password':password})
+r = requests.post(f'{host}/login', data={'username': username, 'password': password})
 token = r.json()['access_token']
 headers = {"Authorization": f"Bearer {token}"}
