52476924829f08f84ade2290fc836ea96e58a767
igol84 <igolub84@gmail.com>
2021-10-09 18:55:01 +0300

    api sli by base

--- prjstore/db/api/components/sale_line_item.py
+++ prjstore/db/api/components/sale_line_item.py
@@ -1,12 +1,12 @@
 from prjstore.db.api.components.base import APIBase
-from prjstore.schemas import sale_line_item as schemas
+from prjstore.schemas import sale_line_item as sch
 
 
 class API_SaleLineItem(
-    APIBase[schemas.CreateSaleLineItem, schemas.CreateSaleLineItem, schemas.ShowSaleLineItemWithItem]):
+    APIBase[sch.CreateSaleLineItem, sch.CreateSaleLineItem, sch.ShowSaleLineItemWithItem]):
     prefix = 'sale_line_item'
-    schema = schemas.ShowSaleLineItemWithItem
-    list_schema = schemas.ListSaleLineItems
+    schema = sch.ShowSaleLineItemWithItem
+    list_schema = sch.ListSaleLineItems
     keys = ['sale_id', 'item_id', 'sale_price']
 
     def __init__(self, headers):
