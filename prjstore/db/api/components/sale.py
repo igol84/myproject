454a99f22ed7636e34f2a2f470e8e029276f2495
igol84 <igolub84@gmail.com>
2021-10-10 14:38:39 +0300

    api sale

--- prjstore/db/api/components/sale.py
+++ prjstore/db/api/components/sale.py
@@ -1,7 +1,5 @@
-import requests
 from prjstore.db.api.components.base import APIBase
 from prjstore.schemas import sale as schemas
-from prjstore.db.api import settings
 
 
 class API_Sale(APIBase[schemas.CreateSale, schemas.UpdateSale, schemas.ShowSaleWithSLIs]):
@@ -11,11 +9,3 @@ class API_Sale(APIBase[schemas.CreateSale, schemas.UpdateSale, schemas.ShowSaleW
 
     def __init__(self, headers):
         super().__init__(headers)
-
-    def update(self, new_obj: schemas.UpdateSale) -> schemas.ShowSaleWithSLIs:
-        r = requests.put(f"{settings.host}/{self.prefix}/",
-                         data=new_obj.json(), headers=self.headers)
-        if r.status_code != 202:
-            raise ConnectionError(r.text)
-        else:
-            return self.schema(**r.json())
